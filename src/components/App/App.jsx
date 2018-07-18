5f2dadc9b5745cb57e7ab290459beae3569ee574
jhamPac <cloud@kevia.me>
2018-07-18 08:57:49 -0700

    css and import App;

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import './App.css';
 
-const App = () => <div className="app-container" />;
+const App = () => <div className="app-container">This is the Application</div>;
 
 export default App;
